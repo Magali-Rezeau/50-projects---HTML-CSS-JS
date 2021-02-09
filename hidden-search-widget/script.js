518011dd1761bb6df1e0005e061f82311d54a8c1
Magali Rézeau <magalirezeau@gmail.com>
2021-02-09 10:12:32 +0100

    second solution for search-widget added

--- hidden-search-widget/script.js
+++ hidden-search-widget/script.js
@@ -1,10 +1,14 @@
 const btnSearch = document.querySelector('.search-bar__icon-search');
 const searchBar = document.querySelector('.search-bar');
+const input = document.querySelector('input');
 
 btnSearch.addEventListener('click', () => {
-  if (searchBar.classList.contains('active')) {
-    searchBar.classList.remove('active');
-  } else {
-    searchBar.classList.add('active');
-  }
+  searchBar.classList.toggle('active');
+  input.focus();
+  // if (searchBar.classList.contains('active')) {
+  //   searchBar.classList.remove('active');
+  // } else {
+  //   searchBar.classList.add('active');
+  // }
+  // input.focus();
 });
