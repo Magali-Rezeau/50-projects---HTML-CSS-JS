b9a2aecd909ac8caecbed75ff03ee89878c8a212
Magali Rézeau <magalirezeau@gmail.com>
2021-02-09 20:38:55 +0100

    project resolved

--- blurry-loading/script.js
+++ blurry-loading/script.js
@@ -1,15 +1,17 @@
 const loaderText = document.querySelector('.loader__text');
 const loader = document.querySelector('.loader');
 const bg = document.querySelector('.bg');
-const circle = document.querySelector('.circle');
 const face1 = document.querySelector('.loader .face.face1');
 const face2 = document.querySelector('.loader .face.face2');
+
 let load = 0;
 
 const map = (num, in_min, in_max, out_min, out_max) => {
   return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 };
+
 const int = setInterval(timer, 100);
+
 function timer() {
   load++;
   if (load > 99) {
@@ -17,12 +19,8 @@ function timer() {
     face2.style.animation = 'none';
     clearInterval(int);
   }
-  
+
   loaderText.innerText = `${load}%`;
   loader.style.opacity = map(load, 0, 100, 1, 0);
   bg.style.filter = `blur(${map(load, 0, 100, 30, 0)}px)`;
 };
-
-
-
-
